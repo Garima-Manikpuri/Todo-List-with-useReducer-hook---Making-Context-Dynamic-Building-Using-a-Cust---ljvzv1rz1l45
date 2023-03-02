d356e70dd7a688c6beae1488d98797f1cc51a147
Garima-Manikpuri <108508073+Garima-Manikpuri@users.noreply.github.com>
2023-03-02 16:27:35 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,34 +1,21 @@
-import React,{useState} from "react";
+import React, { useState } from "react"
 
-const AddTodo = ({ dispatch }) => {
+const AddTodo = ({dispatch}) => {
 
-    const [taskArr,setTaskArr] =useState([]);
-    const [inputVal,setVal] = useState("");
-    const [DateId,setId] = useState( new Date().getTime().toString());
+    const [tempInput, setTempInput] = useState('');
 
-    function submitHandler (e) {
-        e.preventDefault();  
-        setId( new Date().getTime().toString()) ;
-        setTaskArr(
-            [...taskArr,
-                {id:DateId,title:inputVal}
-            ]
-        )
-        dispatch({type:"ADD",payload:{title:inputVal,id:DateId}});
-        setVal("");
-    }
-
-
-    function inputFun(e) {
-        setVal(e.target.value);
+    const submitFn = (e) => {
+        e.preventDefault();
+        dispatch({type: 'ADDTODO', payload: tempInput});
+        setTempInput('');
     }
 
     return (
         <>
-        <form id="todo-form" onSubmit={submitHandler}>
-            <input id="todo-input" type="textarea" required onChange={inputFun} value={inputVal}/>
-            <button type="submit">submit</button>
-        </form>
+            <form id="todo-form" onSubmit={submitFn}>
+                <input type="text" onChange={(e) => setTempInput(e.target.value)} id="todo-input" value={tempInput}/>
+                <button type="submit">Add To List</button>
+            </form>
         </>
     )
 }
